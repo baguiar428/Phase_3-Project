e3bbf9691539bb1f512deef45e55ae67e765b700
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-11-16 16:47:35 -0500

    Created components for victory, defeat and battle

---
+++ src/components/Defeat.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Defeat(){
+    return(
+
+    );
+}
+
+export default Defeat;
---
+++ src/components/Victory.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Victory() {
+    return (
+
+    );
+}
+
+export default Victory;
+
