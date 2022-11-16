30232d679a45a499a0f7a9856d81dbfafcb9b3e0
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-11-15 20:15:40 -0500

    created nav and link routes for about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import {GrHomeRounded} from "react-icons/gr"
+import SideBarIcon from "./SideBarIcon";
 
 function About() {
 
     const navigate = useNavigate();
 
+    function home() {
+        navigate("/")
+    }
+
     return (
-        <p className="text-white">Test</p>
+        <div
+        className="fixed top-0 left-0 h-screen w-16 m-0
+          flex flex-col
+          bg-gray-900 text-white shadow-lg">
+        <div onClick={home}>
+      <SideBarIcon onClick={home} icon={<GrHomeRounded size='32'/>} />
+      </div>
+      </div>
     );
 }
 
