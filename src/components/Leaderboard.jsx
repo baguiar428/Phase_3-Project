f09f2c113f098c2d404f5bfd5f4badb6d75004a3
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-11-16 16:53:50 -0500

    voodoo

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import {GrHomeRounded, GrInfo} from "react-icons/gr"
+import { GrHomeRounded, GrInfo } from "react-icons/gr";
 import SideBarIcon from "./SideBarIcon";
 
 function Leaderboard() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  function home() {
+    navigate("/");
+  }
 
-    function home() {
-        navigate("/")
-    }
+  function about() {
+    navigate("/about");
+  }
 
-    function about() {
-        navigate("/about")
-      }
-
-    return (
-        <div
-        className="fixed top-0 left-0 h-screen w-16 m-0
+  return (
+    <div
+      className="fixed top-0 left-0 h-screen w-16 m-0
           flex flex-col
-          bg-gray-900 text-white shadow-lg">
-        <div onClick={home}>
-      <SideBarIcon onClick={home} icon={<GrHomeRounded size='32'/>} />
+          bg-gray-900 text-white shadow-lg"
+    >
+      <div onClick={home}>
+        <SideBarIcon onClick={home} icon={<GrHomeRounded size="32" />} />
       </div>
       <div onClick={about}>
-      <SideBarIcon onClick={about} icon={<GrInfo size='32'/>} />
+        <SideBarIcon onClick={about} icon={<GrInfo size="32" />} />
       </div>
-      </div>
-    );
-
+    </div>
+  );
 }
 
 export default Leaderboard;
