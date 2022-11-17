75d0f3acfbbbba5f02f75071db5515bb394ac1ae
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-11-17 15:54:22 -0500

    Added sidebar nav to choose name

--- src/components/CharacterName.jsx
+++ src/components/CharacterName.jsx
@@ -1,32 +1,65 @@
-import React, {useState}from 'react'
-import {useNavigate} from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GrHomeRounded, GrInfo } from "react-icons/gr";
+import SideBarIconHome from "./SideBarIconHome";
+import SideBarIconAbout from "./SideBarIconAbout";
 
 //need to setup css for this
-function CharacterName(){
-    const [charName, setCharName] = useState();
-    const navigate = useNavigate();
-    
-    function handleSubmit(){
-        navigate(`/characters`, {state: {name: charName}})
-    }
+function CharacterName() {
+  const [charName, setCharName] = useState();
+  const navigate = useNavigate();
 
-    function handleChange(e){
-        setCharName(e.target.value)
-    }
+  function home() {
+    navigate("/");
+  }
 
-    return(
-        <div className="display: block text-center text-green-500
-                font-retro text-5xl pl-10 pr-10 ml-10 mr-10">
-            <p></p>
-            <form onSubmit = {handleSubmit}>
-                <div className="flex flex-col justify-center items-center space-y-5 h-screen">
-                <label>What is your name, Hero?</label>
-                <input className='text-black' type='text' placeholder='Enter Name...' value={charName} onChange={handleChange}/>
-                <button type='submit'>Submit </button>
-                </div>
-            </form>
+  function about() {
+    navigate("/about");
+  }
+
+  function handleSubmit() {
+    navigate(`/characters`, { state: { name: charName } });
+  }
+
+  function handleChange(e) {
+    setCharName(e.target.value);
+  }
+
+  return (
+    <>
+      <div
+        className="fixed top-0 left-0 h-screen w-16 m-0
+          flex flex-col
+          bg-gray-900 text-white shadow-lg"
+      >
+        <div onClick={home}>
+          <SideBarIconHome onClick={home} icon={<GrHomeRounded size="32" />} />
         </div>
-    )
+        <div onClick={about}>
+          <SideBarIconAbout onClick={about} icon={<GrInfo size="32" />} />
+        </div>
+      </div>
+      <div
+        className="display: block text-center text-green-500
+                font-retro text-5xl pl-10 pr-10 ml-10 mr-10"
+      >
+        <p></p>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col justify-center items-center space-y-5 h-screen">
+            <label>What is your name, Hero?</label>
+            <input
+              className="text-black"
+              type="text"
+              placeholder="Enter Name..."
+              value={charName}
+              onChange={handleChange}
+            />
+            <button type="submit">Submit </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default CharacterName
+export default CharacterName;
