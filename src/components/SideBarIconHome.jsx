0af125d826bd3992eec7ddc102617856892ea681
Jonathan Bruckman <jbruckman86@gmail.com>
2022-11-17 13:57:04 -0500

    added monster images

--- src/components/SideBarIconHome.jsx
+++ src/components/SideBarIconHome.jsx
@@ -5,7 +5,7 @@ return (
     <div className="sidebar-icon group">
     {icon}
 
-    <span class="sidebar-tooltip group-hover:scale-100">
+    <span className="sidebar-tooltip group-hover:scale-100">
         {text}
     </span>
 </div>
