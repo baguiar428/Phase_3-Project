2d9fe8c08530c42aec41a3dc5727ac281cd13c55
Alex Ju <aju217@gmail.com>
2022-11-17 19:03:18 -0500

    setup leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { GrHomeRounded, GrInfo } from "react-icons/gr";
 import SideBarIconHome from "./SideBarIconHome";
@@ -6,6 +6,7 @@ import SideBarIconAbout from "./SideBarIconAbout";
 
 function Leaderboard() {
   const navigate = useNavigate();
+  const [leaderBoard, setLeaderBoard] = useState([])
 
   function home() {
     navigate("/");
@@ -15,19 +16,51 @@ function Leaderboard() {
     navigate("/about");
   }
 
+  useEffect(()=>{
+    fetch('http://localhost:9292/heros')
+    .then(resp => resp.json())
+    .then(data => setLeaderBoard(data))
+  },[])
+
+  console.log(leaderBoard)
+  const highScores = leaderBoard ? leaderBoard.map((leader) => {
+    return (
+    <tr>
+      <td>{leader.name}</td>
+      <td></td>
+      <td>{leader.wins}</td>
+    </tr>
+  
+  )}) : null
+
   return (
-    <div
-      className="fixed top-0 left-0 h-screen w-16 m-0
-          flex flex-col
-          bg-gray-900 text-white shadow-lg"
-    >
-      <div onClick={home}>
-        <SideBarIconHome onClick={home} icon={<GrHomeRounded size="32" />} />
+    <>
+      <div
+        className="fixed top-0 left-0 h-screen w-16 m-0
+            flex flex-col
+            bg-gray-900 text-white shadow-lg"
+      >
+        <div onClick={home}>
+          <SideBarIconHome onClick={home} icon={<GrHomeRounded size="32" />} />
+        </div>
+        <div onClick={about}>
+          <SideBarIconAbout onClick={about} icon={<GrInfo size="32" />} />
+        </div>
       </div>
-      <div onClick={about}>
-        <SideBarIconAbout onClick={about} icon={<GrInfo size="32" />} />
+      <div className = "font-retro text-3xl flex justify-center text-white">
+      <table>
+          <thead>
+            <tr>
+              <td>Name:</td>
+              <td>Wins</td>
+            </tr>
+          </thead>
+          <tbody>
+            {highScores}
+          </tbody>
+        </table>
       </div>
-    </div>
+    </>
   );
 }
 
